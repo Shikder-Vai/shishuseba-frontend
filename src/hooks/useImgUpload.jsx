75dcc56501e7a418889b45c8a9acb4bee8de7b8d
Shikder-Vai <mdnurujjaman987@gmail.com>
2025-10-01 01:27:42 +0600

    img size reduce before upload added

--- src/hooks/useImgUpload.jsx
+++ src/hooks/useImgUpload.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import imageCompression from 'browser-image-compression';
 import { imgbbKey } from './useImgbb';
 
 const useImgUpload = () => {
@@ -7,14 +8,26 @@ const useImgUpload = () => {
 
   const uploadImg = async (imageFile) => {
     setIsUploading(true);
-    const formData = new FormData();
-    formData.append('image', imageFile);
 
-    const imgbbAxios = axios.create({
-      withCredentials: false // Explicitly set withCredentials to false
-    });
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+    };
 
     try {
+      console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+
+      const compressedFile = await imageCompression(imageFile, options);
+      console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`);
+
+      const formData = new FormData();
+      formData.append('image', compressedFile);
+
+      const imgbbAxios = axios.create({
+        withCredentials: false // Explicitly set withCredentials to false
+      });
+
       const res = await imgbbAxios.post(`https://api.imgbb.com/1/upload?key=${imgbbKey}`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
