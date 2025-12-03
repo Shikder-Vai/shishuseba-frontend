f0f1cebb3dfff773ec9e934f82aaeaf3535d13ca
Shikder-Vai <mdnurujjaman987@gmail.com>
2025-12-03 15:06:28 +0600

    somthing updated

--- src/Dashboard/LandingPage/LandingPageList.jsx
+++ src/Dashboard/LandingPage/LandingPageList.jsx
@@ -154,9 +154,7 @@ const LandingPageList = () => {
                   )}
                   {
                     <Link
-                      to={`${import.meta.env.LANDING_PAGE_URL}/offer/${
-                        page._id
-                      }`}
+                      to={`http://localhost:5173/offer/${page._id}`}
                       target="_blank"
                       className="text-green-500 hover:text-green-600 ml-2"
                     >
